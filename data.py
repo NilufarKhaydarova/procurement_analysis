a83b224d82f00f5c7c05f1bd51c11a7554ee486b
NILUFAR KHAYDAROVA <38408269+NilufarKhaydarova@users.noreply.github.com>
2022-11-09 16:01:27 +0500

    initial commit

---
+++ data.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+data_path = 'data_dx'
+
+names = ['']
+
+def load_data():
+
+    for name in names:  
+        globals()[name] = pd.read_csv(data_path+name+'.dat')
+
+    
+
+
+
+        
+
+
+
